a92e90d6c25ccf8d050bbdc8463e0743bb943e31
Skeletrox <android.svr@gmail.com>
2016-08-18 19:30:49 +0530

    Add files via upload
    For all TamBrahms [and anyone who has to say anything repeatedly]
    
    Something I coded last avani avittam xD

---
+++ CounterTimer.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <conio.h>
+#include <time.h>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+    int val=0, MAX;         //turns out max was a keyword. Hmm.
+    time_t start, finish;
+    double avg;             //Answers the famous "How fast do you say your mantras???" question.
+    cout << "\nEnter max value:\t";
+    cin >> MAX;
+    cout << "\nPress any key when ready to begin timing\n";
+    while(!getch());
+    start = time(NULL);
+    while (val<MAX)
+    {
+        while(getch())
+        {
+            val++;
+            cout << val;
+            Beep(900, 250);
+            cout <<"\t";
+            break;
+        }
+    }
+    finish = time(NULL);
+    avg = (double) difftime(finish, start)/MAX;
+    cout << "\nMax value achieved\n";
+    Beep(1900, 750);
+    cout << "\nAverage time per iteration is \t" << avg << " seconds" << endl;    getch();
+    return 0;
+}
